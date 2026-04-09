617dc76ce598453f66cbe5f462fc21d2d665a1a7
Codacy Archive <codacy-archive@local>
2026-04-09 00:20:11 +0000

    snapshot 2026-04-09T00:20:11.072683+00:00

--- docs/conf.py
+++ docs/conf.py
@@ -2,30 +2,31 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Sphinx config file."""
+"""Sphinx configuration file.
 
-# See doc at:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+Sphinx doc at:
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
 
 import datetime
 import pathlib
 import sys
 
+_HERE = pathlib.Path(__file__).resolve().parent
+_ROOT_DIR = _HERE.parent
+sys.path.insert(0, str(_ROOT_DIR))
+sys.path.insert(0, str(_HERE / '_ext'))
+
+from _bootstrap import get_version  # noqa: E402
+
 PROJECT_NAME = "psutil"
 AUTHOR = "Giampaolo Rodola"
 THIS_YEAR = str(datetime.datetime.now().year)
-HERE = pathlib.Path(__file__).resolve().parent
-ROOT_DIR = HERE.parent
-
-sys.path.insert(0, str(ROOT_DIR))
-from _bootstrap import get_version  # noqa: E402
-
 VERSION = get_version()
 
-
-sys.path.insert(0, str(HERE / '_ext'))
+# =====================================================================
+# Extensions
+# =====================================================================
 
 extensions = [
     "sphinx.ext.extlinks",
@@ -40,41 +41,63 @@ extensions = [
     "field_role",
 ]
 
+# =====================================================================
+# Project metadata
+# =====================================================================
+
 project = PROJECT_NAME
 author = AUTHOR
-version = VERSION
-release = VERSION
+version = release = VERSION
+copyright = f"2009-{THIS_YEAR}, {AUTHOR}"  # shown in the footer
+
+# =====================================================================
+# Cross-references and external links
+# =====================================================================
+
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3', None),
+    "python": ("https://docs.python.org/3", None),
 }
 extlinks = {
-    'gh': ('https://github.com/giampaolo/psutil/issues/%s', '#%s'),
+    "gh": ("https://github.com/giampaolo/psutil/issues/%s", "#%s"),
 }
-htmlhelp_basename = f"{PROJECT_NAME}-doc"
-copybutton_exclude = '.linenos, .gp'
 
-# --- paths
+# =====================================================================
+# Paths
+# =====================================================================
 
-templates_path = ['_templates']
-html_static_path = ['_static']
-html_css_files = [
-    'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
-    'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
-    'css/custom.css',
-]
-html_js_files = [
-    "js/highlight-repl.js",
-    "js/external-urls.js",
-    ("js/theme-toggle.js", {"defer": "defer"}),
-]
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+html_static_path = ["_static"]
+exclude_patterns = ["_build"]
 
-# --- theming / visual
+# =====================================================================
+# HTML
+# =====================================================================
 
-html_title = "psutil"
-html_logo = "_static/images/logo-psutil.svg"
+html_title = PROJECT_NAME
+html_logo = "_static/images/logo-psutil.svg"  # shown in the sidebar
 html_favicon = "_static/images/favicon.svg"
-html_theme = 'sphinx_rtd_theme'
-pygments_style = "monokai"  # https://pygments.org/styles/
-copyright = f"2009-{THIS_YEAR}, {AUTHOR}"  # shown in the footer
 html_last_updated_fmt = "%b %d, %Y"  # shown in the footer
+
+# =====================================================================
+# Plugins
+# =====================================================================
+
+copybutton_exclude = ".linenos, .gp"
+
+# =====================================================================
+# Theming
+# =====================================================================
+
+html_theme = "sphinx_rtd_theme"
+
+if html_theme == "sphinx_rtd_theme":
+    templates_path = ["_templates", "_static/images"]
+    pygments_style = "monokai"  # https://pygments.org/styles/
+    html_css_files = [
+        "css/custom.css",
+    ]
+    html_js_files = [
+        "js/highlight-repl.js",
+        "js/external-urls.js",
+        ("js/theme-toggle.js", {"defer": "defer"}),
+        ("js/sidebar-close.js", {"defer": "defer"}),
+    ]
